f4c3533700384250e248883163b208226bcffd7e
Gary Byrne <garyb1@live.ie>
2020-11-25 10:40:33 +0000

    add jokes with module

---
+++ jokes.js
@@ -0,0 +1,536 @@
+export default [
+    [
+      "Relationship status?",
+      "I'll leave the relations to the database."
+    ],
+    [
+      "How do you get the code for the bank vault?",
+      "You checkout their branch."
+    ],
+    [
+      "How did the developer announce their engagement?",
+      "They return ed true!"
+    ],
+    [
+      "Why did the security conscious engineer refuse to pay their dinner bill?",
+      "Because they could not verify the checksum."
+    ],
+    [
+      "What do you call a busy waiter?",
+      "A server."
+    ],
+    [
+      "What do you call an idle server?",
+      "A waiter."
+    ],
+    [
+      "How many Prolog programmers does it take to change a lightbulb?",
+      "Yes."
+    ],
+    [
+      "I’ve been hearing news about this big boolean.Huge if true."
+    ],
+    [
+      "What diet did the ghost developer go on?",
+      "Boolean"
+    ],
+    [
+      "Why was the developer unhappy at their job?",
+      "They wanted arrays."
+    ],
+    [
+      "Why did 10 get paid less than \"10\"?",
+      "There was workplace inequality."
+    ],
+    [
+      "Why was the function sad after a successful first call?",
+      "It didn’t get a callback."
+    ],
+    [
+      "Why did the angry function exceed the callstack size?",
+      "It got into an Argument with itself"
+    ],
+    [
+      "Whats the object-oriented way to become wealthy?",
+      "Inheritance"
+    ],
+    [
+      "Why did the developer ground their kid?",
+      "They weren't telling the truthy"
+    ],
+    [
+      "What did the array say after it was extended?",
+      "Stop objectifying me."
+    ],
+    [
+      "!false",
+      "It's funny 'cause it's true."
+    ],
+    [
+      "Where did the parallel function wash its hands?",
+      "Async"
+    ],
+    [
+      "I'm starting a band called HTML Encoder",
+      "Looking to buy a guitar &amp;"
+    ],
+    [
+      "Why did the functions stop calling each other?",
+      "Because they had constant arguments."
+    ],
+    [
+      "What's the second movie about a database engineer called?",
+      "The SQL."
+    ],
+    [
+      "Why doesn't Hollywood make more Big Data movies?",
+      "NoSQL."
+    ],
+    [
+      "What did the spider do on the computer?",
+      "Made a website!"
+    ],
+    [
+      "What did the computer do at lunchtime?",
+      "Had a byte!"
+    ],
+    [
+      "What does a baby computer call his father?",
+      "Data!"
+    ],
+    [
+      "Why did the computer keep sneezing?",
+      "It had a virus!"
+    ],
+    [
+      "What is a computer virus?",
+      "A terminal illness!"
+    ],
+    [
+      "I never tell the same joke twiceI have a DRY sense of humor."
+    ],
+    [
+      "Why was the computer freezing?",
+      "It left its Windows open!"
+    ],
+    [
+      "Why was there a bug in the computer?",
+      "Because it was looking for a byte to eat?"
+    ],
+    [
+      "Why did the computer squeak?",
+      "Because someone stepped on its mouse!"
+    ],
+    [
+      "What do you get when you cross a computer and a life guard?",
+      "A screensaver!"
+    ],
+    [
+      "Where do all the cool mice live?",
+      "In their mousepads!"
+    ],
+    [
+      "What do you get when you cross a computer with an elephant?",
+      "Lots of memory!"
+    ],
+    [
+      "Java truly is an OOP language...As in: OOPs I used Java!"
+    ],
+    [
+      "How do programming pirates pass method parameters?",
+      "Varrrrarrrgs."
+    ],
+    [
+      "How do programming shepherds count their flock?",
+      "With lambda functions"
+    ],
+    [
+      "What airline do developers prefer when they're in a rush?",
+      "Delta."
+    ],
+    [
+      "How did pirates collaborate before computers ?",
+      "Pier to pier networking."
+    ],
+    [
+      "Why don't bachelors like Git?",
+      "Because they are afraid to commit."
+    ],
+    [
+      "A SQL query goes into a bar, walks up to two tables and asks:",
+      "Can I JOIN you?"
+    ],
+    [
+      "How does a developer make a cheer?",
+      "[\"hip\",\"hip\"] // (hip hip array!)"
+    ],
+    [
+      "Why was the developer's family upset with them at dinner?",
+      "They forgot to git squash before going home"
+    ],
+    [
+      "What did JavaScript call his son?",
+      "JSON!"
+    ],
+    [
+      "What did the proud React component say to its child?",
+      "I've got to give you props"
+    ],
+    [
+      "What did the server say to his client who was having a bad day?",
+      "Everything's going to be 200"
+    ],
+    [
+      "Why did the developer go broke?",
+      "Because they used up all their cache"
+    ],
+    [
+      "Are computers dangerous?",
+      "Nah, they don't byte. They just nibble a bit."
+    ],
+    [
+      "How did the mafioso kill the Node server?",
+      "Tie await to it and let it async."
+    ],
+    [
+      "You know what the best thing about booleans is?",
+      "Even if you are wrong, you are only off by a bit."
+    ],
+    [
+      "Why couldn’t the user update a file on a shared server?",
+      "They didn’t have the write permissions"
+    ],
+    [
+      "What do you do when you can't understand your husband's behavior?",
+      "man man"
+    ],
+    [
+      "What do you call a doctor who fixes websites?",
+      "A URLogist"
+    ],
+    [
+      "How many developers does it take to change a light bulb?",
+      "None. It's a hardware issue"
+    ],
+    [
+      "Why do programmers always mix up Halloween and Christmas?",
+      "Because 31 OCT == 25 DEC"
+    ],
+    [
+      "Why do kayakers make bad programmers?",
+      "Because they're afraid of waterfall."
+    ],
+    [
+      "What are computers' favorite snacks?",
+      "Microchips, phish sticks, and cookies. But just a few bytes of each."
+    ],
+    [
+      "What do computers love to do at the beach?",
+      "Put on some spam block for protection so they can safely surf the net while catching some .WAVs!"
+    ],
+    [
+      "What do you call a computer that sings?",
+      "A-dell."
+    ],
+    [
+      "What's a compiler developer's favorite spice?",
+      "Parsley."
+    ],
+    [
+      "When do front end developers go out to eat?",
+      "On their lunch."
+    ],
+    [
+      "A SQL developer walked into a NoSQL bar.",
+      "They left because they couldn't find a table."
+    ],
+    [
+      "How do you help JS errors?",
+      "You console them!"
+    ],
+    [
+      "Why don't parents teach their kids about regular expressions?",
+      "Because they don't want them playing with matches"
+    ],
+    [
+      "Why didn't the <div /> get invited to the dinner party?",
+      "Because it had no class!"
+    ],
+    [
+      "Why aren't cryptocurrency engineers allowed to vote?",
+      "Because they're miners!"
+    ],
+    [
+      "Why did the constant break up with the variable?",
+      "Because they changed."
+    ],
+    [
+      "Why did the database administrator leave his wife?",
+      "She had one-to-many relationships."
+    ],
+    [
+      "Asynchronous JavaScript is amazing.I Promise you, await and see."
+    ],
+    [
+      "What did the Class say in court when put on trial?",
+      "I strongly object!"
+    ],
+    [
+      "Why do Java developers wear glasses?",
+      "Because they don't C#!"
+    ],
+    [
+      "What are the three hardest problems in computer science?",
+      "Naming things and off-by-one errors"
+    ],
+    [
+      "What did the fruit basket say to the developer?",
+      "I hope you're ready for some pear programming!"
+    ],
+    [
+      "How does a sysadmin keep a fire going?",
+      "They rotate the logs."
+    ],
+    [
+      "I've got a great UDP joke but I'm afraid you wouldn't get it..."
+    ],
+    [
+      "A programmer was arrested for writing unreadable code. They refused to comment."
+    ],
+    [
+      "There are 10 types of people in this world, those who understand binary and those who don't."
+    ],
+    [
+      "Why do all HTML emails get blocked?",
+      "Because they are all <span />."
+    ],
+    [
+      "What did the process say after working in an infinite loop all day?",
+      "I need a break."
+    ],
+    [
+      "An Agent died unexpectedly. How was the crime solved?",
+      "By looking at the Stack Trace."
+    ],
+    [
+      "Why did the document store go out of business?",
+      "It had NoSQL."
+    ],
+    [
+      "Why can't SQL and NoSQL Developers date one other?",
+      "Because they don't agree on relationships."
+    ],
+    [
+      "Why is Python like the Soviet Union?",
+      "Because it has no private fields"
+    ],
+    [
+      "Where did the API go to eat?",
+      "To the RESTaurant"
+    ],
+    [
+      "Why shouldn't you trust Matlab developers?",
+      "Because they're always plotting something."
+    ],
+    [
+      "Why did the developer have to quit smoking?",
+      "Because they couldn't afford to pay the new syntax."
+    ],
+    [
+      "How does a programmer open a jar for their significant other?",
+      "They install Java"
+    ],
+    [
+      "What did the psychic say to the developers?",
+      "I see dev people."
+    ],
+    [
+      "Where does the pirate stash all of their digital treasures?",
+      "RAR"
+    ],
+    [
+      "What is React's favorite movie genre?",
+      "Suspense"
+    ],
+    [
+      "Why couldn't the React component understand the joke?",
+      "Because it didn't get the context."
+    ],
+    [
+      "What did XHR say to AJAX when it thought it was being a Mean Girl?",
+      "Stop trying to make fetch happen!"
+    ],
+    [
+      "What was Grace Hopper's favorite car?",
+      "VW Bug"
+    ],
+    [
+      "What sits on a pirate's shoulder and calls, \"Pieces of seven, Pieces of seven\"?",
+      "Parroty error."
+    ],
+    [
+      "What is a pirate's favorite programming language?",
+      "You'd think it was R, but a pirate's first love is Objectively C."
+    ],
+    [
+      "Why did the programmer come home crying?\"",
+      "His friends were always boolean him."
+    ],
+    [
+      "Knock Knock!",
+      "An async function Who's there?"
+    ],
+    [
+      "What PostgreSQL library should Python developers use for adult-oriented code?",
+      "psycoPG13"
+    ],
+    [
+      "The next time you're using Safari or Firefox and it's running slowly, you can say to yourself, \"[I could've had a V8](https://www.youtube.com/watch?v=ld8eAH0MW00)\"."
+    ],
+    [
+      "What accommodations did the JavaScript developer request at the hotel?",
+      "A room with a Vue."
+    ],
+    [
+      "Where do developers drink?",
+      "The Foo bar"
+    ],
+    [
+      "Why do assembly programmers need to know how to swim?",
+      "Because they work below C level."
+    ],
+    [
+      "Who used the internet before it was cool?",
+      "Httpsters"
+    ],
+    [
+      "What kind of computer can hold a musical note?",
+      "A Dell."
+    ],
+    [
+      "Why did the web developer always go to the wrong hotel room?",
+      "They were in room 301."
+    ],
+    [
+      "How do you stop a web developer stealing your stuff?",
+      "Write 403 on it."
+    ],
+    [
+      "Why are machine learning models so fit?",
+      "Because they do weight training."
+    ],
+    [
+      "Why did Gargamel shut down the internet?",
+      "Because he didn't want people SMURFING the web!"
+    ],
+    [
+      "What did the command line die of?", "A Terminal illness."
+    ],
+    [
+      "Did you hear what the clumsy cryptographer did to their password?", "Made a hash of it."
+    ],
+    [
+      "Why are keyboards always working so hard?", "Cause they have two shifts!"
+    ],
+    [
+      "What are clouds made of?", "Mostly linux servers."
+    ],
+    [
+      "How does Mr. Potato Head (dev edition 0.0.1) remove his mustache?", "git stache pop"
+    ],
+    [
+      "Why can't you use 'Soup' as your password?", "Because it isn't stroganoff"
+    ],
+    [
+      "Why do developers use mechanical keyboards?", "To strongly type their code."
+    ],
+    [
+      "A new database query walks into a bar.The server says \"Sorry, cache only.\""
+    ],
+    [
+      "What's the best tool for automatically ignoring long email threads about tech buzzwords?\"Block-chain\""
+    ],
+    [
+      "What is a developer's favorite country song?", "Hello World - by Lady Antebellum"
+    ],
+    [
+      "Why was nobody given food at the developer conference?", "It was a serverless function!"
+    ],
+    [
+      "Why did the developer cancel their dinner plans?", "They were unable to fulfil peer dependencies"
+    ],
+    [
+      "Why did the functional programmer finally move out of their house?", "For(e) closure"
+    ],
+    [
+      "How do JavaScript developers break up?",
+      "They always promise to callback"
+    ],
+    [
+      "Why do developers mixup Terminals and Polygraphs?",
+      "Because they both can see a lie (CLI)"
+    ],
+    [
+      "Did you hear about the programmer that was scared of IDEs?",
+      "They retreated back into their shell"
+    ],
+    [
+      "What do you call optimistic front-end developers?",
+      "Stack half-full developers."
+    ],
+    [
+      "Chuck Norris can take a screenshot of his blue screen."
+    ],
+    [
+      "Have you heard the one about the Corduroy pillow?",
+      "It's making HEADLINES!"
+    ],
+    [
+      "Hey officer! How did the hackers escape?",
+      "No idea. They just ransomware."
+    ],
+    [
+      "Why can’t data engineers become hat makers?",
+      "They can only guarantee two thirds of a CAP!"
+    ],
+    [
+      "How did the hippie learn about database transactions?",
+      "By taking ACID"
+    ],
+    [
+      "Why is it called the Dark Ages?",
+      "There were a lot of KNIGHTS!"
+    ],
+    [
+      "What did the Network Administrator say when they caught a nasty virus?",
+      "It hurts when IP"
+    ],
+    [
+      "Which programming language is the shortest?",
+      "HTML. Because it doesn't have a neck between its <head> and <body>."
+    ],
+    [
+      "What good can come of 2989 witches casting a hex?", "None, it is always 0xBAD"
+    ],
+    [
+      "Did you hear about the witch who was off by two when casting a hex?", "They failed to make the target DEAD and made them DEAF instead!"
+    ],
+    [
+      "I went to a street where the houses were numbered 8k, 16k, 32k, 64k, 128k, 256k and 512k.It was a trip down Memory Lane."
+    ],
+    [
+      "Lisp programmers don't make prank calls. They make FUNCALLs"
+    ],
+    [
+      "Why do Front-End Developers eat lunch alone?", "Because, they don't know how to join tables."
+    ],
+    [
+      "What advice do you give to a JS developer who has never played baseball?", "Try catch."
+    ],
+    [
+      "Got any funny DNS jokes?", "Yeah, but it may take 24 hours to get it."
+    ],
+    [
+      "Where do programmer dad's store their jokes?", "In a dad-a-base."
+    ]
+  ]
